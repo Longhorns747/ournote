d42ab5addb530ee1cd3fa07f6403a9fd5ab2832b
Ethan Shernan <eshernan3@gatech.edu>
2013-07-01 21:29:47 -0700

    Commit initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ournote::Application.config.secret_key_base = 'a07d561197d98868bdb5047d8e81401e0b2db6b0f323c83d67eb22d89d4d0c60d70e8b99ae2fbb53fa7e4492a915afe6d7b6fbad2817600fa735fb67f954236a'
