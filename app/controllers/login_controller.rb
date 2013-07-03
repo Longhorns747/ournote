f454ff48a59c09cce2d85b9ddee1bbe3531e49ee
Ethan Shernan <eshernan3@gatech.edu>
2013-07-02 22:29:20 -0700

    Added invalid login notification

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,12 @@
 class LoginController < ApplicationController
 	def create
 		@user = User.find_by(username: params[:login][:username], password: params[:login][:password])
-		session[:username] = @user.username
-		redirect_to @user
+
+		if(@user.nil?)
+			render 'invalid'
+		else
+			session[:user_id] = @user.id
+			redirect_to @user
+		end
 	end
 end
