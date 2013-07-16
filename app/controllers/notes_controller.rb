ba8e0d1a64c12d804d67a1c524afd27fa2e698d4
Ethan Shernan <eshernan3@gatech.edu>
2013-07-15 19:47:33 -0700

    Can now edit notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,6 +3,10 @@ class NotesController < ApplicationController
 		@note = Note.new
 	end
 	
+	def edit
+  		@note = User.find(params[:user_id]).notes.find(params[:id])
+	end
+
 	def create
 		@user = User.find(params[:user_id])
 		@note = @user.notes.create(params[:note].permit(:topic, :c_name, :content))
@@ -15,5 +19,15 @@ class NotesController < ApplicationController
 		@note.destroy
 
 		redirect_to user_path(@user)
+	end
+
+	def update
+		@note = User.find(params[:user_id]).notes.find(params[:id])
+ 
+  		if @note.update(params[:note].permit(:topic, :c_name, :content))
+    		redirect_to @note.user
+  		else
+    		render 'edit'
+  		end
 	end
 end
