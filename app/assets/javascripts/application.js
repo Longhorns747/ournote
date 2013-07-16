9e580785839aa9bad5a6da11501cacdd8967b188
Ethan Shernan <eshernan3@gatech.edu>
2013-07-15 23:35:05 -0700

    Quick fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require lightbox_me
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
