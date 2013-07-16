67696d72da45f18e3677ffb0736eb8490df3ed70
Ethan Shernan <eshernan3@gatech.edu>
2013-07-15 18:57:21 -0700

    Added deleting notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,4 +8,12 @@ class NotesController < ApplicationController
 		@note = @user.notes.create(params[:note].permit(:topic, :c_name, :content))
 		redirect_to user_path(@user)
 	end
+
+	def destroy
+		@user = User.find(params[:user_id])
+		@note = @user.notes.find(params[:id])
+		@note.destroy
+
+		redirect_to user_path(@user)
+	end
 end
