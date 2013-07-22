4c8444afc336f8b26261e42e211844f150c28adb
Ethan Shernan <eshernan3@gatech.edu>
2013-07-21 21:09:23 -0700

    Commented some ruby

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,21 +8,25 @@ class NotesController < ApplicationController
 	end
 
 	def create
+		#Get the user and then build the note
 		@user = User.find(params[:user_id])
 		@note = @user.notes.build(params[:note].permit(:c_name))
 
+		#Split the topics by comma delimiter, then build
 		topics = params[:note].permit(:topics)['topics'].split(',')
-
 		topics.each do |topic|
 			@note.topics.build(:topic => topic.strip)
 		end
 
 		if @note.valid?
+			#If the note is valid, now we need to check the file
 			if NoteContent.new(params[:note].permit(:content)).valid?
+				#If the file is valid, save the note and associate the file with it
 				@note.save
 				@note.note_contents.create(params[:note].permit(:content))
 				redirect_to user_path(@user)
 			else
+				#Otherwise redirect invalid
 				redirect_to user_path(@user, :invalid => ['set'])
 			end
 
@@ -32,6 +36,7 @@ class NotesController < ApplicationController
 	end
 
 	def destroy
+		#Find the note and user and destroy them. Associations should be destroyed by cascade.
 		@user = User.find(params[:user_id])
 		@note = @user.notes.find(params[:id])
 		@note.destroy
@@ -41,18 +46,23 @@ class NotesController < ApplicationController
 
 	def update
 		@note = User.find(params[:user_id]).notes.find(params[:id])
- 
+
+		#If theere is a class, update the note
   		if @note.update(params[:note].permit(:c_name))
 
+  			#Add topics
 			topics = params[:note].permit(:topics)['topics'].split(',')
 
 			topics.each do |topic|
 				@note.topics.create(:topic => topic.strip)
 			end
 
+			#Add files
 			note_content = @note.note_contents.build(params[:note].permit(:content))
 
+			#Make sure we only save the file if it exists and is valid
 			if note_content.nil?
+				#If file is valid, save it, otherwise redirect with an error
 	  			if note_content.valid?
 	  				note_content.save
 					redirect_to @note.user
